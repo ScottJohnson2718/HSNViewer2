c678c9da57f939915155453d34a2b17f1ffb5eb0
Scott Johnson <abbydog@cfl.rr.com>
2013-03-16 22:01:04 -0400

    First commit
    Content Readers will load the dude if I cut paste XNB output from
    Skinned Sample.  Current assets are hardcoded to f:\Content.
    Does not render the character yet but I think that is graphics problem
    or just now viewing the character.

---
+++ HSNViewer2/Bone.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace BetterSkinned
+{
+    /// <summary>
+    /// Bones in this model are represented by this class, which
+    /// allows a bone to have more detail associatd with it.
+    /// 
+    /// This class allows you to manipulate the local coordinate system
+    /// for objects by changing the scaling, translation, and rotation.
+    /// These are indepenent of the bind transformation originally supplied
+    /// for the model. So, the actual transformation for a bone is
+    /// the product of the:
+    /// 
+    /// Scaling
+    /// Bind scaling (scaling removed from the bind transform)
+    /// Rotation
+    /// Translation
+    /// Bind Transformation
+    /// Parent Absolute Transformation
+    /// 
+    /// </summary>
+	public class Bone
+    {
+        #region Fields
+
+        /// <summary>
+        /// Any parent for this bone
+        /// </summary>
+        private Bone parent = null;
+
+        /// <summary>
+        /// The children of this bone
+        /// </summary>
+        private List<Bone> children = new List<Bone>();
+
+        /// <summary>
+        /// The bind transform is the transform for this bone
+        /// as loaded from the original model. It's the base pose.
+        /// I do remove any scaling, though.
+        /// </summary>
+        private Matrix bindTransform = Matrix.Identity;
+
+        /// <summary>
+        /// The bind scaling component extracted from the bind transform
+        /// </summary>
+        private Vector3 bindScale = Vector3.One;
+
+        /// <summary>
+        /// Any translation applied to the bone
+        /// </summary>
+        private Vector3 translation = Vector3.Zero;
+
+        /// <summary>
+        /// Any rotation applied to the bone
+        /// </summary>
+        private Quaternion rotation = Quaternion.Identity;
+
+        /// <summary>
+        /// Any scaling applied to the bone
+        /// </summary>
+        private Vector3 scale = Vector3.One;
+
+        #endregion 
+
+        #region Properties
+
+        /// <summary>
+        /// The bone name
+        /// </summary>
+		public string Name = "";
+
+        /// <summary>
+        /// The bone bind transform
+        /// It is a transform from the boneSpace to WorldSpace when in the bind pose.  
+        /// Bone space in the bind pose is also called SkinSpace
+        /// </summary>
+        public Matrix BindTransform { get {return bindTransform;} }
+
+        /// <summary>
+        /// Inverse of absolute bind transform for skinnning
+        /// </summary>
+        public Matrix SkinTransform { get; set; }
+
+        /// <summary>
+        /// Bone rotation
+        /// </summary>
+		public Quaternion Rotation {get {return rotation;} set {rotation = value;}}
+
+        /// <summary>
+        /// Any translations
+        /// </summary>
+		public Vector3 Translation {get {return translation;} set {translation = value;}}
+
+        /// <summary>
+        /// Any scaling
+        /// </summary>
+        public Vector3 Scale { get { return scale; } set { scale = value; } }
+
+        /// <summary>
+        /// The parent bone or null for the root bone
+        /// </summary>
+        public Bone Parent { get { return parent; } }
+
+        /// <summary>
+        /// The children of this bone
+        /// </summary>
+        public List<Bone> Children { get { return children; } }
+
+        /// <summary>
+        /// The bone absolute transform.  This is a bone to world matrix
+        /// </summary>
+        public Matrix AbsoluteTransform = Matrix.Identity;
+        public Matrix AbsoluteSkinTransform = Matrix.Identity;
+
+        /// <summary>
+        /// Matrix used to that external sources (head tracking) can affect the bone
+        /// </summary>
+        //public Matrix Modifier;
+
+
+        #endregion
+
+        #region Operations
+
+        /// <summary>
+        /// Constructor for a bone object
+        /// </summary>
+        /// <param name="name">The name of the bone</param>
+        /// <param name="bindTransform">The initial bind transform for the bone</param>
+        /// <param name="parent">A parent for this bone</param>
+        public Bone(string name, Matrix bindTransform, Bone parent)
+        {
+            this.Name = name;
+            this.parent = parent;
+            if (parent != null)
+                parent.children.Add(this);
+
+            // I am not supporting scaling in animation in this
+            // example, so I extract the bind scaling from the 
+            // bind transform and save it. 
+
+            this.bindScale = new Vector3(bindTransform.Right.Length(),
+                bindTransform.Up.Length(), bindTransform.Backward.Length());
+
+            bindTransform.Right = bindTransform.Right / bindScale.X;
+            bindTransform.Up = bindTransform.Up / bindScale.Y;
+            bindTransform.Backward = bindTransform.Backward / bindScale.Y;
+            this.bindTransform = bindTransform;
+
+            //Modifier = Matrix.Identity;
+            // Set the skinning bind transform
+            // That is the inverse of the absolute transform in the bind pose
+
+            ComputeAbsoluteSkinTransform();
+            SkinTransform = Matrix.Invert(AbsoluteSkinTransform);
+
+        }
+
+        /// <summary>
+        /// Compute the absolute skin transformation for this bone.
+        /// This contains the bind transform
+        /// </summary>
+        public void ComputeAbsoluteSkinTransform()
+        {
+            // The only caller of this function is the constructor and there are no
+            // rotations or translations at that point so could remove first term
+            Matrix transform = BoneToParent() * BindTransform;
+
+            if (Parent != null)
+            {
+                // This bone has a parent bone
+                // This assumes that the parent bone has already been computed.  I don't
+                // know why the original author can make that assumption.
+                AbsoluteSkinTransform = transform * Parent.AbsoluteSkinTransform;
+            }
+            else
+            {   // The root bone
+                AbsoluteSkinTransform = transform;
+            }
+        }
+
+        /// <summary>
+        /// Compute the absolute transformation for this bone.
+        /// This is actually a bone to world matrix
+        /// </summary>
+        public void ComputeAbsoluteTransform()
+        {
+            Matrix transform = BoneToParent();// *BindTransform;
+
+            if (Parent != null)
+            {
+                // This bone has a parent bone
+                // This assumes that the parent bone has already been computed.  I don't
+                // know why the original author can make that assumption.
+                AbsoluteTransform = transform * Parent.AbsoluteTransform;
+            }
+            else
+            {   // The root bone
+                AbsoluteTransform = transform;
+            }
+        }
+
+        /// <summary>
+        /// Compute BoneToParent transform but leave out skinning matrix or bind matrix
+        /// </summary>
+        /// <returns></returns>
+        public Matrix BoneToParent()
+        {
+            Matrix transform = Matrix.CreateFromQuaternion(Rotation) * Matrix.CreateTranslation(Translation);
+            return transform;
+        }
+
+        /// <summary>
+        ///  Compute a bone to world matrix.  Make sure there is no bind matrix in it
+        /// </summary>
+        /// <returns></returns>
+        public Matrix BoneToWorld()
+        {
+            return AbsoluteTransform;
+
+        //    Matrix transform = BoneToParent();
+
+        //    if (Parent != null)
+        //    {
+        //        // This bone has a parent bone
+        //        transform = transform * Parent.BoneToWorld();       // wow, could I write this any slower?  Would like it to work first. X-O
+        //    }
+
+        //    return transform;
+        }
+
+        /// <summary>
+        /// This sets the rotation and translation such that the
+        /// rotation times the translation times the bind after set
+        /// equals this matrix. This is used to set animation values.
+        /// </summary>
+        /// <param name="m">A matrix include translation and rotation</param>
+        public void SetCompleteTransform(Matrix m)
+        {
+            Matrix setTo = m;// *Matrix.Invert(BindTransform);
+
+            Translation = setTo.Translation;
+            Rotation = Quaternion.CreateFromRotationMatrix(setTo);
+        }
+
+        public void ForwardKinematics(Matrix boneToParent)
+        {
+            ForwardKinematicsRecurse(this, boneToParent);
+        }
+
+        private void ForwardKinematicsRecurse(Bone b, Matrix parentToWorld)
+        {
+            b.AbsoluteTransform = b.BoneToParent() * parentToWorld;
+
+            foreach (Bone child in b.children)
+            {
+                ForwardKinematicsRecurse(child, b.AbsoluteTransform);
+            }
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ HSNViewer2/MainWindow.xaml.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics; // for stopwatch
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+//using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+using System.IO;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+using WpfHostedXna;
+using Primitives3D;
+
+using AnimationAux;
+using BetterSkinned;
+
+namespace HSNViewer2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //ContentBuilder contentBuilder;
+        ContentManager contentManager;
+     
+        //private CubePrimitive cube;
+
+        // We use a Stopwatch to track our total time for cube animation
+        private Stopwatch watch = new Stopwatch();
+
+        // A yaw and pitch applied to the second viewport based on input
+        //private float yaw = 0f;
+        //private float pitch = 0f;
+
+        // The color applied to the cube in the second viewport
+        Color cubeColor = Color.Red;
+
+        Camera camera;
+
+        /// <summary>
+        /// The animated model we are displaying
+        /// </summary>
+        private AnimatedModel model = null;
+
+        AnimationPlayer player;
+
+        private double totalGameTime = 0.0;
+ 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Got here");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Invoked after either control has created its graphics device.
+        /// </summary>
+        private void loadContent(object sender, GraphicsDeviceEventArgs e)
+        {
+            // Start the watch now that we're going to be starting our draw loop
+            watch.Start();
+
+           // contentBuilder = new ContentBuilder();
+
+            camera = new Camera(e.GraphicsDevice);
+
+
+          // Load the model we will display
+            //model = new AnimatedModel("Victoria-hat-tpose");
+            model = new AnimatedModel("dude");
+
+            // Default to the directory which contains our content files.
+            //string assemblyLocation = Assembly.GetExecutingAssembly().Location;
+            //string relativePath = System.IO.Path.Combine(assemblyLocation, "../../../../Content");
+            //string contentPath = System.IO.Path.GetFullPath(relativePath);
+
+            // Since the build project doesn't work, use the builder
+            //contentBuilder.Add("f:\\Content\\Dude.fbx", "Model", null, "ModelProcessor");
+
+    // THe content project doesn't work away from the XNA Game project.  It has a content reference to the BetterSkinned
+    // Content project.  I haven't found a way to get the content project to build in the context of this Solution.
+    // Manually copied the results of the content pipeline to a Content directory.  A clean of the build may remove
+    // it entirely.  This project is currently broken.
+            contentManager = new ContentManager(xnaControl1.Services, "f:\\Content");
+
+            Cursor = Cursors.Wait;
+
+            // We can only use the builder when I figure out how to get it to use the custom content. Plus
+            // I don't want it to build the model every time the program runs.
+            // Build this new model data.
+            //string buildError = contentBuilder.Build();
+
+            //if (string.IsNullOrEmpty(buildError))
+            //{
+                // If the build succeeded, use the ContentManager to
+                // load the temporary .xnb file that we just created.
+               model.LoadContent(contentManager);
+            //}
+            //else
+            //{
+                // If the build failed, display an error message.
+            //    MessageBox.Show(buildError, "Error");
+            //}
+            Cursor = Cursors.Arrow;
+
+
+            // Load the model that has an animation clip it in
+            //dance = new AnimatedModel("Victoria-hat-dance");
+            //dance.LoadContent(Content);
+
+            // Obtain the clip we want to play. I'm using an absolute index, 
+            // because XNA 4.0 won't allow you to have more than one animation
+            // associated with a model, anyway. It would be easy to add code
+            // to look up the clip by name and to index it by name in the model.
+            AnimationClip clip = model.Clips[0];
+
+            // And play the clip
+            player = model.PlayClip(clip);
+            player.Looping = true;
+        }
+
+        /// <summary>
+        /// Invoked when our first control is ready to render.
+        /// </summary>
+        private void xnaControl1_RenderXna(object sender, GraphicsDeviceEventArgs e)
+        {
+            // Fake the XNA update call
+            totalGameTime +=  watch.Elapsed.TotalSeconds;
+            TimeSpan elapsedSpan = TimeSpan.FromSeconds( watch.Elapsed.TotalSeconds );
+            TimeSpan totalSpan = TimeSpan.FromSeconds( totalGameTime );
+            GameTime gameTime = new GameTime( totalSpan, elapsedSpan );
+
+            Update(gameTime);
+            camera.Update(e.GraphicsDevice, gameTime);
+
+            // Now actually do render stuff
+
+            e.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // Compute some values for the cube rotation
+            float time = (float)watch.Elapsed.TotalSeconds;
+
+            // Create the world-view-projection matrices for the cube and camera
+            //Matrix world = Matrix.CreateFromYawPitchRoll(yaw, pitch, roll);
+           // Matrix world = Matrix.Identity;
+            //Matrix view = Matrix.CreateLookAt(new Vector3(0, 0, 2.5f), Vector3.Zero, Vector3.Up);
+           // Matrix projection = Matrix.CreatePerspectiveFieldOfView(1, e.GraphicsDevice.Viewport.AspectRatio, 1, 10);
+
+            // Get the color from our sliders (don't have a slider)
+            //Color color = new Color((float)rComponent.Value, (float)gComponent.Value, (float)bComponent.Value);
+            Color color = Color.Aqua;
+
+            // Draw a cube
+            //cube.Draw(world, view, projection, color);
+
+
+            model.Draw(e.GraphicsDevice, camera, Matrix.Identity);
+        }
+
+        // This is coming from the render tick which is different than actual XNA but we are
+        // faking an actual XNA Game object so it will have to do.
+        private void Update(GameTime deltaTime)
+        {
+            model.Update(deltaTime, Vector3.Zero);
+ 
+        }
+    }
+}
